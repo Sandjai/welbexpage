8e6c17b20104e31852d13483ce79641e960795ed
Grigory Semakin <grinsdot1@gmail.com>
2023-03-12 17:14:15 +0300

    setup env

---
+++ src/constants/fixtures.js
@@ -0,0 +1,25 @@
+export const menuItems__header = [
+  { id: 1, item: "Услуги" },
+  { id: 2, item: "Виджеты" },
+  { id: 3, item: "Интеграции" },
+  { id: 4, item: "Кейсы" },
+  { id: 5, item: "Сертификаты" },
+];
+
+export const menuItems__footer = [
+  { id: 1, item: "Расчёт стоимости" },
+  { id: 2, item: "Услуги" },
+  { id: 3, item: "Виджеты" },
+  { id: 4, item: "Интеграции" },
+  { id: 5, item: "Наши клиенты" },
+  { id: 6, item: "Кейсы" },
+  { id: 7, item: "Благодарственные письма" },
+  { id: 8, item: "Сертификаты" },
+  { id: 9, item: "Блог на Youtube" },
+  { id: 10, item: "Вопрос / Ответ" },
+];
+
+export const aboutItems = [
+  { id: 1, item: "Партнёрская программа" },
+  { id: 1, item: "Вакансии" },
+];
